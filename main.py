24b97f2c6c4ebbe4e7d39dd1cb3804d5da6faa3e
Alexandr Silyanov <142574130+Sidyaniv@users.noreply.github.com>
2024-04-04 20:26:09 +0300

    ---

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
-# from scipy.ndimage import rotate
-from src.shadow_height_estimator import (estimate_building_heights,
-                                        take_indices_shadows,
+from scipy.ndimage import rotate
+from src.shadow_height_estimator import (
                                         attach_heights, 
                                         )
 from src.visualization_masks import (draw_masks,
                                     draw_random_masks,
                                     draw_sample_building_shadow,
                                     )
-from src.annotations.annotation import ann_and_images
+from src.annotations.annotation import (ann_and_images,
+                                        coco2masks)
 import numpy as np
 
 from typing import Tuple, Dict
@@ -28,63 +28,29 @@ BROWN = (np.uint8(128), np.uint8(64), np.uint8(48))
 BLUE = (np.uint8(0), np.uint8(0), np.uint8(255))
 COLORS = np.array([BROWN, BLUE])
 
-# image/s, anns = ann_and_images(coco_annotation_file=annot_file, images_directory=images_directory)
-# a = images[0]
-# b = anns[0]
-# print(a)
 
-from PIL import Image
+image, masks = coco2masks(coco_annotation_file=annot_file, images_directory=images_directory)
 
+# random_segment = draw_random_masks(image[0], masks)
+segment = draw_masks(image[0], masks, COLORS)
 
-
-def get_image_center(image_path) -> Tuple[float, float]:
-    image = Image.open(image_path)
-    
-    width, height = image.size
-    
-    image.close()
-    return (width / 2, height / 2)
-
-
-def rotate_points(shawdow_points, center, angle):
-    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
-                                [np.sin(angle), np.cos(angle)]])
-
-    rotated_points = []
-    for point in shawdow_points:  
-        dx = point[0] - center[0]
-        dy = point[1] - center[1]
-
-        # Создаем матрицу поворота
-        
-        # Применяем матрицу поворота к разности координат
-        rotated_dx, rotated_dy = np.dot(rotation_matrix, [dx, dy])
-
-        # Добавляем результат к координатам центра поворота
-        new_x = center[0] + rotated_dx
-        new_y = center[1] + rotated_dy
-        new_point = (new_x, new_y)
-
-        rotated_points.append(new_point)
-        
-    return rotated_points
+# from PIL import Image
 
 
 
-center = get_image_center(image_directory)
-print(center)
 
-# from scipy import ndimage, datasets
-# import matplotlib.pyplot as plt
-# fig = plt.figure(figsize=(10, 3))
-# ax1, ax2, ax3 = fig.subplots(1, 3)
-# img = images[0]
-# img_45 = ndimage.rotate(img, 45, reshape=False)
-# ax1.imshow(img, cmap='gray')
-# ax1.set_axis_off()
-# ax2.imshow(img_45, cmap='gray')
-# ax2.set_axis_off()
-# # ax3.imshow(full_img_45, cmap='gray')
-# ax3.set_axis_off()
-# fig.set_layout_engine('tight')
-# plt.show()
+# # from scipy import ndimage, datasets
+# # import matplotlib.pyplot as plt
+# # fig = plt.figure(figsize=(10, 3))
+# # ax1, ax2, ax3 = fig.subplots(1, 3)
+# # img = images[0]
+# # img_45 = ndimage.rotate(img, 45, reshape=False)
+# # ax1.imshow(img, cmap='gray')
+# # ax1.set_axis_off()
+# # ax2.imshow(img_45, cmap='gray')
+# # ax2.set_axis_off()
+# # # ax3.imshow(full_img_45, cmap='gray')
+# # ax3.set_axis_off()
+# # fig.set_layout_engine('tight')
+# # plt.show()
+
--- src/annotations/annotation.py
+++ src/annotations/annotation.py
@@ -8,6 +8,23 @@ def load_image(image_info, images_directory):
     return cv2.imread(os.path.join(images_directory, image_info['file_name']))
 
 
+def load_masks(image_id, coco):
+    annotations = coco.loadAnns(coco.getAnnIds(imgIds=image_id))
+    return [coco.annToMask(ann) * ann['category_id'] for ann in annotations]
+
+
+def coco2masks(coco_annotation_file: str, images_directory: str):
+    coco = COCO(coco_annotation_file)
+
+    image_list = []
+    mask_list = []
+    for image_id in coco.getImgIds():
+        image_list.append(load_image(coco.loadImgs(image_id)[0], images_directory))
+        mask_list.extend(load_masks(image_id, coco))
+
+    return np.array(image_list), np.array(mask_list)
+
+
 def load_annotations(image_id, coco):
     
     annotations = coco.loadAnns(coco.getAnnIds(imgIds=image_id))
