7458e6734e21dc7618e8507d0f97d66f1d34015c
Alexandr Silyanov <142574130+Sidyaniv@users.noreply.github.com>
2024-04-28 16:14:33 +0300

    Initial commit

--- src/shadow_height_estimator.py
+++ src/shadow_height_estimator.py
@@ -1,15 +1,23 @@
 import math
 import numpy as np
 
-# from scipy.ndimage import rotate
 from typing import Dict, List, Union, Tuple
 from src.utils_for_estimate import find_heights_and_shadows, pixel2metr, get_points
-# from src.annotations.annotation import anns_and_images
 
 
 def estimate_buildings_height(rotated_shadow_points: Dict[int, List[Tuple[float]]],
                               scale,
                               sun_elevation_angle,) -> Dict[int, float]:
+    """estimate_buildings_height - estimate the height of the building in meters
+
+    Args:
+        rotated_shadow_points (Dict[int, List[Tuple[float]]]): 
+        scale (float): meters in pixels
+        sun_elevation_angle (int): sunangle
+
+    Returns:
+        Dict[int, float]
+    """
     dict_of_heights = {}
     for key, points in rotated_shadow_points.items():
         # распаковываем координаты, лежащие в кортежах, в список
@@ -19,7 +27,7 @@ def estimate_buildings_height(rotated_shadow_points: Dict[int, List[Tuple[float]
 
         shadow_length_pixels = np.max(list_of_y) - np.min(list_of_y)
         shadow_length_meters = pixel2metr(shadow_length_pixels, scale)
-        building_height = math.tan(math.radians(sun_elevation_angle)) * shadow_length_meters
+        building_height = abs(math.tan(sun_elevation_angle) * shadow_length_meters)
 
         dict_of_heights[key] = round(building_height, 2)
 
@@ -29,26 +37,39 @@ def estimate_buildings_height(rotated_shadow_points: Dict[int, List[Tuple[float]
 def rotate_point(x_coordinate: float,
                  y_coordinate: float,
                  angle: float,
-                 center: float
                  ) -> Tuple[float]:
+    """rotate_point - rotate 1 point
+
+    Args:
+        x_coordinate (float): x axis coordinate
+        y_coordinate (float): y axis coordinate
+        angle (float): angle of rotate basis
+
+    Returns:
+        Tuple[float]
+    """
     rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
                                 [np.sin(angle), np.cos(angle)]])
 
-    dx = x_coordinate - center[0]
-    dy = y_coordinate - center[1]
+    rotated_dx, rotated_dy = np.dot(rotation_matrix, [x_coordinate, y_coordinate])
 
-    rotated_dx, rotated_dy = np.dot(rotation_matrix, [dx, dy])
-
-    new_x = round(center[0] + rotated_dx, 2)
-    new_y = round(center[1] + rotated_dy, 2)
+    new_x = round(rotated_dx, 2)
+    new_y = round(rotated_dy, 2)
 
     return new_x, new_y
 
 
 def rotate_points(points: Dict[int, List[Tuple[float]]],
-                  center: Tuple[float],
                   rotate_angle: float) -> Dict[int, List[Tuple[float]]]:
+    """rotate_points - Consider the coordinates of the points relative to the rotated basis
 
+    Args:
+        points (Dict[int, List[Tuple[float]]]): list of points
+        rotate_angle (float): angle of rotate basis
+
+    Returns:
+        Dict[int, List[Tuple[float]]]
+    """
     dict_of_points = {}
     for key, rot_points in points.items():
         dict_of_points[key] = []
@@ -57,7 +78,6 @@ def rotate_points(points: Dict[int, List[Tuple[float]]],
             coordinates = rotate_point(x_coordinate=point[0],
                                        y_coordinate=point[1],
                                        angle=rotate_angle,
-                                       center=center
                                        )
             dict_of_points[key].append(coordinates)
 
@@ -65,7 +85,7 @@ def rotate_points(points: Dict[int, List[Tuple[float]]],
 
 
 def rotate_angle(sun_azimuth_angle: float) -> float:
-    return sun_azimuth_angle + 90
+    return np.deg2rad(sun_azimuth_angle + 90) 
 
 
 def attach_heights(
@@ -75,15 +95,11 @@ def attach_heights(
     """attach heights from shadows to building
 
     Args:
-        indices_shadows (Dict[int, float]): indices of shadows
-        estimated_building_heights (Tuple[list]): two lists -
-        1.height of building
-        2.building index
-
+        indices_shadows: Dict[int, float] - the ratio of the shadow to the height of its building 
     Returns:
         Dict[str, Dict[int, List[float]], str, Dict[int, List[int]]]:
           {'buildings heights': {index of building: estimated height},
-          'building-shadow: {index of building: index of shadow}
+          'building shadow: {index of building: index of shadow}
           }
     """
     building_heights, building_shadows = {}, {}
@@ -91,52 +107,48 @@ def attach_heights(
 
     for i, height in enumerate(heights):
         building_heights.update({building_indices[i]: height})
-        # ищем индекс тени для определённой высоты
+        # ищем индекс тени относящийся к определённой высоте
         shadow_index = [key for key, value in indices_shadows.items() if value == height]
         building_shadows.update({building_indices[i]: shadow_index})
 
     return {'building_heights': building_heights, 'building_shadow': building_shadows}
 
 
-def buildings_info(
-    hyperparameters
-) -> Dict[int, Union[float, int]]:
+def buildings_info(hyperparameters) -> Dict[int, Union[float, int]]:
     """
     Creates a dictionary where the key is the number `int` of the building's
-    COCO annotation in the masks `np.ndarray`, and the value is the building mask corresponding
-    to this index and a list of heights `List[float]` that may belong to this annotation.
+    COCO annotation, and the value is the building annotation corresponding
+    to this index and height that may belong to this annotation.
 
     As input, it accepts annotation, the value of the sun's azimuth angle
     in degrees `float`, a scale `int` that indicates how many meters are in one
-    pixel of the image, the sun's elevation angle `float` in degrees, the threshold `int` and
-    center of image. For searching for the building closest to the shadow for marking. Heights are calculated
+    pixel of the image, the sun's elevation angle `float` in degrees, the threshold `int`.
+    For searching for the building closest to the shadow for marking. Heights are calculated
     from shadow annotations and matched to buildings using a threshold.
-    It is important to choose the right threshold for calculating building heights.
 
     Args:
         Dict[
             'anns': COCO annotations
-            'sun_azimuth_angle': float
+            'sun_azimuth_angle': float in degrees
             'scale': int
-            'sun_elevation_angle': float
+            'sun_elevation_angle': float in degrees
             'threshold': int
-            'img_center': float
             ]
 
     Returns:
-        {uid: {'annotation': COCO annotation, 'heights': List[float]}}
+        {uid: {'annotation': COCO annotation, 'heights': float}}
     """
+    hyperparameters['sun_azimuth_angle'] = np.deg2rad(hyperparameters['sun_azimuth_angle'])
+    hyperparameters['sun_elevation_angle'] = np.deg2rad(hyperparameters['sun_elevation_angle'])
 
-    shadows_points = get_points(hyperparameters['anns'])[0]
-    building_points = get_points(hyperparameters['anns'])[1]
+    shadows_points, building_points = get_points(hyperparameters['anns'])
 
     angle_of_rotate = rotate_angle(hyperparameters['sun_azimuth_angle'])
 
     rotated_points_of_shadows = rotate_points(points=shadows_points,
-                                              center=hyperparameters['img_center'],
                                               rotate_angle=angle_of_rotate)
+
     rotated_points_of_buildings = rotate_points(points=building_points,
-                                                center=hyperparameters['img_center'],
                                                 rotate_angle=angle_of_rotate)
 
     shadows_heigth = estimate_buildings_height(rotated_points_of_shadows,
@@ -147,79 +159,17 @@ def buildings_info(
     heights_and_shadows = find_heights_and_shadows(rotated_points_of_shadows,
                                                    rotated_points_of_buildings,
                                                    shadows_heigth,
-                                                   hyperparameters['threshold']
                                                    )
 
-    building_heights_shadows = attach_heights(
-        estimate_buildings_height(
-            rotated_points_of_shadows,
-            hyperparameters['scale'],
-            hyperparameters['sun_elevation_angle'],
-        ),
-
-        heights_and_shadows
-    )
+    building_heights_shadows = attach_heights(shadows_heigth, 
+                                              heights_and_shadows)
 
     result_dict = {}
 
-    for idx_building, heights in building_heights_shadows['building_heights'].items():
+    for idx_building, heigths in building_heights_shadows['building_heights'].items():
         result_dict[idx_building] = {
             'anns': hyperparameters['anns'][idx_building],
-            'heights': heights,
+            'heigths': heigths,
         }
 
     return result_dict
-
-# from utils_for_estimate import (get_points,
-#                                     get_image_center,
-#                                     find_heights_and_shadows,
-#                                     extreme_points,
-#                                     extra_rotate_angle,
-#                                     find_horizontal_area,
-#                                     )
-# from annotations.annotation import anns_and_images
-# import numpy as np
-
-# dataset_dir = '../3d_tools_dataset/'
-# annot_file = dataset_dir + 'annotations/instances_default.json'
-# images_directory = dataset_dir + 'image_png/'
-# image_directory = dataset_dir + 'image_png/test_1.png'
-
-# SCALE = np.float16(0.1)
-# SUN_AZIMUTH_ANGLE = np.uint8(100)
-# SUN_ELEVATION_ANGLE = np.uint8(30)
-# ROTATE_ANGLE = SUN_AZIMUTH_ANGLE + 90.0
-# THRESHOLD = np.uint8(5)
-# BROWN = (np.uint8(128), np.uint8(64), np.uint8(48))
-# BLUE = (np.uint8(0), np.uint8(0), np.uint8(255))
-# COLORS = np.array([BROWN, BLUE])
-
-# image, anns = anns_and_images(coco_annotation_file=annot_file, images_directory=images_directory)
-
-# shadows_points = get_points(anns)[0]
-# building_points = get_points(anns)[1]
-
-# center = get_image_center(image_path=image_directory)
-# rotated_points_of_shadows = rotate_points(points=shadows_points, center=center, rotate_angle=ROTATE_ANGLE)
-# rotated_points_of_buildings = rotate_points(points=building_points, center=center, rotate_angle=ROTATE_ANGLE)
-
-# extra_angles = {}
-# list_of_areas = []
-# for iteration, points_of_shadow in rotated_points_of_shadows.items():
-#     area = find_horizontal_area(points_of_shadow, THRESHOLD)
-#     extr_poi = extreme_points(points_of_shadow, area)
-#     extra_angles[iteration] = extra_rotate_angle(extr_poi)
-    
-# shadows_heigth = estimate_buildings_height(rotated_points_of_shadows, SCALE, SUN_ELEVATION_ANGLE)
-
-# find = find_heights_and_shadows(rotated_points_of_shadows,
-#                                 rotated_points_of_buildings,
-#                                 shadows_heigth,
-#                                 THRESHOLD
-#                                 )
-
-# attach = attach_heights(shadows_heigth, find)
-
-
-# print(extra_angles)
-# print(shadows_heigth)
--- src/utils_for_estimate.py
+++ src/utils_for_estimate.py
@@ -1,36 +1,31 @@
-import math
 import numpy as np
 from typing import Tuple, Dict, List
-from PIL import Image
-
 
 def pixel2metr(pixels, scale: np.float16) -> float:
     return pixels * scale
 
 
-def get_image_center(image_path: str) -> Tuple[float, float]:
-    image = Image.open(image_path)
+def get_points(annotations) -> List[Tuple[float]]:  
+    """get_points - get from annotations separated from each 
+    other points of buildings and shadows
 
-    width, height = image.size
+    Args:
+        annotations (_type_): COCO annotation
 
-    image.close()
-    return (width / 2, height / 2)
-
-
-def get_points(annotations) -> List[Tuple[float]]:  # noqa E501
+    Returns:
+        List[Tuple[float]], List[Tuple[float]] 
+    """
     shawdows_tuples = {}
     buildings_tuples = {}
     for i, ann in enumerate(annotations):
         if ann['category_id'] == 2:
             # Разделяем точки по кортежам
             shawdows_points = ann['segmentation'][0]
-            # shawdows_pairs[i] =shawdows_points
             shawdows_tuples[i] = [(shawdows_points[index], shawdows_points[index + 1]) for index in range(0, len(shawdows_points), 2)]
 
         if ann['category_id'] == 1:
             # Разделяем точки по кортежам
             buildings_points = ann['segmentation'][0]
-            # buildings_pairs[i] = buildings_points
             buildings_tuples[i] = [(buildings_points[index], buildings_points[index + 1]) for index in range(0, len(buildings_points), 2)]
     return shawdows_tuples, buildings_tuples
 
@@ -39,35 +34,44 @@ def find_heights_and_shadows(
     rotated_shawdows_points: Dict[int, List[Tuple[float]]],
     rotated_buildings_points: Dict[int, List[Tuple[float]]],
     estimated_heights: Dict[int, float],
-    threshold: int,
 ) -> Tuple[List[float]]:
+    """find_heights_and_shadows - Looking for the ratio of the building to the appropriate height
+
+    Args:
+        rotated_shawdows_points (Dict[int, List[Tuple[float]]]): Rotated coordinates of shadow points
+        rotated_buildings_points (Dict[int, List[Tuple[float]]]): Rotated coordinates of buildings points
+        estimated_heights (Dict[int, float]): built height
+
+    Returns:
+        Tuple[List[float]]
+    """
     heights = []
     buildings = []
+    rotated_buildings_points_copy = rotated_buildings_points.copy()
     stop = False
     for shadow_key, shadow in rotated_shawdows_points.items():
-        area = required_area(shadow, threshold)
+        area_shadow = required_area(shadow)
 
-        for building_key, building in rotated_buildings_points.items():
+        for building_key, building in rotated_buildings_points_copy.items():
+            area_build = required_area(building)
+            build_length = abs(np.max(area_build[1])) - abs(np.min(area_build[1]))
 
             for point in building:
                 # проверяем входит ли точка здания в указанный диапозон, при положительном исходе выходим из цикла
-                if area[0][1] <= point[0] <= area[0][0]:
-                    if area[1][1] <= point[1] <= area[1][0]:
+                if np.min(area_shadow[0]) <= point[0] <= np.max(area_shadow[0]) :
+                    if np.min(area_shadow[1]) <= point[1] <= np.min(area_shadow[1]) + build_length:
                         heights.append(estimated_heights[shadow_key])
                         buildings.append(building_key)
                         stop = True
-                        del rotated_buildings_points[building_key]
+                        del rotated_buildings_points_copy[building_key]
                         break
             if stop:
                 stop = False
                 break
-
     return heights, buildings
 
 
-# получаем необходимую зону (полосу) в виде координат, ограничивающих её
 def required_area(rotated_shawdow_points: List[Tuple[float]],
-                  threshold: int
                   ) -> Tuple[Tuple[float]]:
     unpacked_list = [item for sublist in rotated_shawdow_points for item in sublist]
 
@@ -77,121 +81,7 @@ def required_area(rotated_shawdow_points: List[Tuple[float]],
     diapason_x = (np.max(x_coordinates), np.min(x_coordinates))
     diapason_y = (np.max(y_coordinates), np.min(y_coordinates))
 
-    x_range = (diapason_x[0] + threshold, diapason_x[1] - threshold)
-    y_range = (diapason_y[0] + threshold, diapason_y[1] - threshold)
+    x_range = (diapason_x[0] , diapason_x[1])
+    y_range = (diapason_y[0] , diapason_y[1])
 
     return x_range, y_range
-
-
-def find_horizontal_area(shadow_points: List[Tuple[float]],
-              threshold: int,) -> Tuple[Tuple[float]]:
-    x_diaposon = required_area(shadow_points, threshold)[0]
-
-    x_max_range = (x_diaposon[0], x_diaposon[0] - 2 * threshold)
-    x_min_range = (x_diaposon[1] + 2 * threshold, x_diaposon[1])
-    return x_max_range, x_min_range
-
-
-def extreme_points(rotated_shawdow_points: List[Tuple[float]],
-                   horizontal_area: Tuple[Tuple[float]]
-                  ) -> Tuple[Tuple[float]]:
-
-    right_range = horizontal_area[0]
-    left_range = horizontal_area[1]
-
-    right_y_max_point = [None, 0]
-    right_y_min_point = [None, 1000000]
-
-    left_y_max_point = [None, 0]
-    left_y_min_point = [None, 1000000]
-
-    for point in rotated_shawdow_points:
-        # находим крайние точки минимального диапозона y
-        if right_range[1] <= point[0] <= right_range[0]:
-            if point[1] > right_y_max_point[1]:
-                right_y_max_point = point
-            if point[1] < right_y_min_point[1]:
-                right_y_min_point = point
-
-        # находим крайние точки максимального диапозона y
-        if left_range[1] <= point[0] <= left_range[0]:
-            if point[1] > left_y_max_point[1]:
-                left_y_max_point = point
-            if point[1] < left_y_min_point[1]:
-                left_y_min_point = point
-    return  [left_y_min_point, right_y_max_point, left_y_max_point, right_y_min_point]
-
-
-def extra_rotate_angle(extreme_points: List[List[Tuple[float]]],):
-    
-# катеты
-    leg_1_x = round(extreme_points[1][0] - extreme_points[0][0], 2)
-    leg_1_y = round(extreme_points[1][1] - extreme_points[0][1], 2)
-
-    leg_2_x = round(extreme_points[2][0] - extreme_points[3][0], 2)
-    leg_2_y = round(extreme_points[2][1] - extreme_points[3][1], 2)
-
-    if leg_1_y <= 0:
-        angle_1 = 180 - (math.atan(leg_1_x / abs(leg_1_y) ) * 180 / 3.14)
-    else: 
-        angle_1 = math.atan(leg_1_x / leg_1_y) * 180 / 3.14
-
-    if leg_2_y <= 0:
-        angle_2 = math.atan(leg_2_x / abs(leg_2_y)) * 180 / 3.14
-    else:
-        angle_2 = math.atan(leg_2_x / leg_2_y) * 180 / 3.14
-
-    return round((angle_1 + angle_2) / 2, 2)
-
-
-# def extreme_points(rotated_shawdow_points: List[Tuple[float]],
-#                   ) -> Tuple[Tuple[float]]:
-#     unpacked_list = [item for sublist in rotated_shawdow_points for item in sublist]
-
-#     x_coordinates = unpacked_list[0::2]
-#     y_coordinates = unpacked_list[1::2]
-
-#     diapason_x = [np.max(x_coordinates), np.min(x_coordinates)]
-#     diapason_y = [np.max(y_coordinates), np.min(y_coordinates)]
-
-#     for index, coordinate in enumerate(x_coordinates):
-#         if coordinate == diapason_x[0]:
-#             x_max_point = (diapason_x[0], y_coordinates[index])
-#         if coordinate == diapason_x[1]:
-#             x_min_point = (diapason_x[1], y_coordinates[index])
-
-#         # if x_max_point and x_min_point:
-#         #     break
-
-
-#     for index, coordinate in enumerate(y_coordinates):
-#         if coordinate == diapason_y[0]:
-#             y_max_point = (x_coordinates[index], diapason_y[0])
-#         if coordinate == diapason_y[1]:
-#             y_min_point = (x_coordinates[index], diapason_y[1])
-
-#         # if y_max_point and y_min_point:
-#             # break
-#     return (x_max_point, x_min_point, y_max_point, y_min_point)
-
-
-# def extra_rotate_angle(extreme_points: Tuple[Tuple[float]],):
-#     x_point_max, x_point_min = extreme_points[0], extreme_points[1]
-#     y_point_max, y_point_min = extreme_points[2], extreme_points[3]
-
-# # катеты
-#     leg_1_2 = round(x_point_min[0] - x_point_max[0], 2)
-#     leg_1_1 = round(x_point_max[1] - x_point_min[1], 2)
-
-#     leg_2_1 = round(y_point_max[1] - y_point_min[1], 2)
-#     leg_2_2 = round(y_point_min[0] - y_point_max[0], 2)
-
-    
-#     angle_x = np.arctan(leg_1_1 / leg_1_2) * 180 / 3.14
-#     angle_y = np.arctan(leg_2_1 / leg_2_2) * 180 / 3.14
-
-#     return (angle_x + angle_y) / 2
-# 1. Находим координаты точки с максимальным y и координаты точки с минимальным y
-# 2. Находим координаты точки с максимальным x и координаты точки с минимальным x
-# 3. Находим 2 точки: 1. Точка, лежащая на оси x уже повернутого изображения и имеющая координату x = y_max[0] и y = y_min[1]. Для y
-#                     2. Точка, лежащая на оси x уже повернутого изображения и имеющая координату x = x_max[0] и y = x_min[1]. Для x
